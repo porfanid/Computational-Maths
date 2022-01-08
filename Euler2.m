498a273dbca52e2aaa2a45f0f297a8efc1e89168
GeiorgeHatz <georgehatziligos51@gmail.com>
2022-01-08 14:16:57 +0200

    added a test euler method

---
+++ Euler2.m
@@ -0,0 +1,105 @@
+function Euler()
+  
+  #---------------- Data ----------------#
+  f1 = 4835/7000
+  f2 = 4835/7000
+  m = 9
+  d=1
+  h=0.0001
+  Iz= 0.038
+  s0 = 4835/1000
+  AM = 4835
+  bs = 3 - (4835/5000)
+  bu = 5 - (4835/5000)
+  
+  t = 0:0.1:30
+  t = 0:0.1:30
+  display(length(t))
+  s1=0
+  #s2= [(f1+f2) - bs*abs(s1)*s1]/9
+  s = zeros(1,length(t))
+  display(length(s))
+  s1 = zeros(1,length(t))
+  s2 = zeros(1,length(t))
+  s1(1) = 0 
+  s(1)=AM/4835
+  # --------------------------- Euler Method 1 --------------------------#
+
+
+
+  # ----- new ------ #
+  #s2= ((f1+f2) - bs*abs(s1)*s1)/m
+  #s2(1) = ((f1+f2)+bs*(abs(s1(1))*s1(1)))
+
+  for k = 1 : length(t)*(t+1)
+    s2(k)= (((f1+f2)+bs*(abs(s1(k))*s1(k))))/m
+    s1(k+1) = s1(k) + h*s2(k)
+  endfor
+
+  figure(1)
+  subplot(1,2,1)
+  plot(s2)
+  subplot(1,2,2)
+  plot(s1)
+  
+  figure(2)
+  for k = 1 : length(t)*(t+1)
+    s(k+1) = s(k)+h*s1(k)
+  endfor
+  plot(s)
+    
+  
+  
+    
+  #---------------- Data ----------------#
+  f1 = 4835/7000
+  f2 = 4835/8000
+  m = 9
+  d=1
+  h=0.0001
+  Iz= 0.038
+  s0 = 4835/1000
+  AM = 4835
+  bs = 3 - (4835/5000)
+  bu = 5 - (4835/5000)
+  
+  t = 0:0.1:30
+  display(length(t))
+  s1=0
+  #s2= [(f1+f2) - bs*abs(s1)*s1]/9
+  s = zeros(1,length(t))
+  display(length(s))
+  s1 = zeros(1,length(t))
+  s2 = zeros(1,length(t))
+  s1(1) = 0 
+  s(1)=AM/4835
+  # --------------------------- Euler Method 2 --------------------------#
+
+
+
+  # ----- new ------ #
+  #s2= ((f1+f2) - bs*abs(s1)*s1)/m
+  #s2(1) = ((f1+f2)+bs*(abs(s1(1))*s1(1)))
+
+  for k = 1 : length(t)*(t+1)
+    s2(k)= (((f1+f2)+bs*(abs(s1(k))*s1(k))))/m
+    s1(k+1) = s1(k) + h*s2(k)
+  endfor
+
+  figure(3)
+  subplot(1,2,1)
+  plot(s2)
+  subplot(1,2,2)
+  plot(s1)
+  
+  figure(4)
+  for k = 1 : length(t)*(t+1)
+    s(k+1) = s(k)+h*s1(k)
+  endfor
+  plot(s)
+  
+  
+    
+  
+
+  endfunction
