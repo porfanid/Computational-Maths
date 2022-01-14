2d72e56adee38fa1ae9e319ce7550279201167ad
GeiorgeHatz <georgehatziligos51@gmail.com>
2022-01-14 22:09:51 +0200

    change some mistakes

--- problem2d.m
+++ problem2d.m
@@ -6,27 +6,30 @@ function problem2d()
   Kps=5;
   Kds=15+(AM/1000);  
   t = 0:0.1:30;
-  Sdes=AM/200;
+  Sdes = AM/200;
   
   s(1)=0;
   s1(1)=0;
-  s2(1)=0;
   Bs = 3 + (AM/5000);
   m=9;
   delta =((Kds+Bs)/m)^2-4*(Kps/m);
-  r1 = ((-Kps/m)+sqrt(delta))/2;
-  r2 = ((-Kps/m)-sqrt(delta))/2;
+  r1 = ((-Kds+Bs/m)+sqrt(delta))/2;
+  r2 = ((-Kps+Bs/m)-sqrt(delta))/2;
   
+  c1 = Sdes*r1/(r2-r1);
+  c2 = -Sdes*(r1/(r2+r1)+1); 
   
   # Begin  #
-  s = Sdes + (-Sdes/(((r1/r2)+1)))*exp(t*(r1/2)) + (-Sdes/((r1/r2)+1))*exp(t*(r2/2));  
-  s1 = Sdes + (r1/2)*(-Sdes/(((r1/r2)+1)))*exp(t*(r1/2)) + (r2/2)*(-Sdes/((r1/r2)+1))*exp(t*(r2/2));
+  
+  
+  s = Sdes + c1*exp(t*(r1))+c2*exp(t*(r2)) ;  
+  s1 = r1*c1*exp(t*(r1))+r2*c2*exp(t*(r2));
   
   
   # Subplot #
   
   figure(1)
-  plot(s1)
+  plot(t,s1)
   title("s' Problem 2 e")
   ylabel(" s'")
   xlabel("t(sec)")
@@ -34,7 +37,7 @@ function problem2d()
   
 
   figure(2)
-  plot(s)
+  plot(t,s)
   title("s Proplem 2 e")
   ylabel(" s ")
   xlabel("t(sec)")
