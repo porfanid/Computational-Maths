7e2ed3f6adaa71a0a64328772f03462b15579b31
GeiorgeHatz <georgehatziligos51@gmail.com>
2022-01-15 03:43:01 +0200

    new

--- Euler2.m
+++ Euler2.m
@@ -1,36 +1,37 @@
-function Euler()
+function Euler2()
   
   #---------------- Data ----------------#
-  f1 = 4835/7000
-  f2 = 4835/7000
-  m = 9
-  d=1
-  h=0.0001
-  Iz= 0.038
-  s0 = 4835/1000
-  AM = 4835
-  bs = 3 - (4835/5000)
-  bu = 5 - (4835/5000)
+  AM = 4835;
+  f1 = AM/7000;
+  f2 = AM/7000;
+  m = 9;
+  d=1;
+  h=0.001;
+  Iz= 0.38;
+  s0 = 4835/1000;
+  AM = 4835;
+  bs = 2.033;
+  bu = 4.033;
   
-  t = 0:0.1:30
-  t = 0:0.1:30
+  t = 0:0.001:30;
   display(length(t))
+  
   # --------------- Transportation ---------------- #
   
-  s1=0
+  s1=0;
   #s2= [(f1+f2) - bs*abs(s1)*s1]/9
-  s = zeros(1,length(t))
-  display(length(s))
-  s1 = zeros(1,length(t))
-  s2 = zeros(1,length(t))
-  s1(1) = 0 
-  s(1)=AM/4835
+  s = zeros(1,30000);
+  s1 = zeros(1,30000);
+  s2 = zeros(1,30000);
+  
+  s1(1) = 0 ;
+  s(1)=AM/1000;
   
   # ---------------- Rational ---------------------- #
   
   
-  w = zeros(1,length(t))
-  w1 = zeros (1,length(t))
+  w = zeros(1,30000);
+  w1 = zeros (1,30000);
   
   
   # ----------- PROGRAM BEGINS ---------------#
@@ -46,60 +47,71 @@ function Euler()
   
   # --------------- Transportation Move ---------------- #
   
-   
-  for k = 1 : length(t)*(t+1)
-    s2(k)= (((f1+f2)+bs*(abs(s1(k))*s1(k))))/m
-    s1(k+1) = s1(k) + h*s2(k)
+  n=3001
+  display(m)
+  for k = 1 : 30000
+    s2(k)= (((f1+f2)-bs*(abs(s1(k))*s1(k)))/m);
+    s1(k+1) = s1(k) + h*s2(k);
   endfor
+
+  
   #plot s" s'
   
-  figure(1)
-  plot(s2)
-  title(" Euler Method s' ")
-  subplot(1,2,1)
-  ylabel("s\"")
-  xlabel("t(sec)")
- 
-  subplot(1,2,2)
-  plot(s1)
-  ylabel("s'")
-  xlabel("t(sec)")
+  display(length(s2))
+  
+  figure(1);
+  subplot(1,2,1);
+  plot(s2);
+  ylabel("s\"");
+  xlabel("t(sec)");
   
   
+  subplot(1,2,2);
+  plot(t,s1);
+  title(" Euler Method s' ");
+  ylabel("s'");
+  xlabel("t(sec)");
   
-  for k = 1 : length(t)*(t)
-    s(k+1) = s(k)+h*s1(k)
+  
+
+  
+  for k = 1 : 30000
+    s(k+1) = s(k)+h*s1(k);
   endfor
 
   #plot s
   
-  figure(2)
-  plot(s)
-  title("Euler Method s")
-  ylabel("s")
-  xlabel("t(sec)")
+  figure(2);
+  plot(t,s);
+  title("Euler Method s");
+  ylabel("s");
+  xlabel("t(sec)");
   
+ 
   # ---------------- Rational Move ---------------------- #
-  w(1) = 0
-  for k = 1: length(t)*(t)
-    w1(k)=((d/2*(f2-f1) - bu*abs(w(k))*w(k)))/Iz
-    w(k+1) = w(k)+h*w1(k)
+  w(1) = 0;
+  for k = 1: 30000
+    w1(k)=((d/2*(f2-f1) - bu*abs(w(k))*w(k)))/Iz;
+    w(k+1) = w(k)+h*w1(k);
   endfor
   
 
-
-  #------ plot w and w' ------#
-  figure(3)
-  subplot(1,2,1)
-  plot(w1)
-  title("Euler Method w")
-  ylabel("w'")
-  xlabel("t(sec)")
   
-  subplot(1,2,2)
-  plot(w)
-  ylabel("w")
-  xlabel("t(sec)")
+  #------ plot w and w' ------#
+  figure(3);
+  subplot(1,2,1);
+  plot(w1);
+
+  ylabel("w'");
+  xlabel("t(sec)");
+  
+
+  subplot(1,2,2);
+
+  plot(t,w);
+  title("Euler Method w");
+  ylabel("w");
+  xlabel("t(sec)");
     
   
   
@@ -108,27 +120,27 @@ function Euler()
   
     
   #---------------- Data 2----------------#
-  f1 = 4835/7000
-  f2 = 4835/8000
-  m = 9
-  d=1
-  h=0.0001
-  Iz= 0.038
-  s0 = 4835/1000
-  AM = 4835
-  bs = 3 - (4835/5000)
-  bu = 5 - (4835/5000)
+  f1 = 4835/7000;
+  f2 = 4835/8000;
+  m = 9;
+  d=1;
+  h=0.001;
+  Iz= 0.38;
+  s0 = 4835/1000;
+  AM = 4835;
+  bs = 3 - (4835/5000);
+  bu = 5 - (4835/5000);
   
-  t = 0:0.1:30
-  display(length(t))
-  s1=0
+  t = 0:0.001:30;
+  display(length(t));
+  s1=0;
   #s2= [(f1+f2) - bs*abs(s1)*s1]/9
-  s = zeros(1,length(t))
-  display(length(s))
-  s1 = zeros(1,length(t))
-  s2 = zeros(1,length(t))
-  s1(1) = 0 
-  s(1)=AM/4835
+  s = zeros(1,30000);
+  
+  s1 = zeros(1,30000);
+  s2 = zeros(1,30000);
+  s1(1) = 0 ;
+  s(1)=AM/1000;
   # --------------------------- Euler Method 2 --------------------------#
 
 
@@ -141,55 +153,58 @@ function Euler()
   # --------------- Transportation Move ---------------- #
   
   
-  for k = 1 : length(t)*(t+1)
-    s2(k)= (((f1+f2)+bs*(abs(s1(k))*s1(k))))/m
-    s1(k+1) = s1(k) + h*s2(k)
+  for k = 1 : 30000
+    s2(k)= (((f1+f2)-bs*(abs(s1(k))*s1(k))))/m;
+    s1(k+1) = s1(k) + h*s2(k);
   endfor
 
   # plot s" and s'
-  figure(4)
-  subplot(1,2,1)
-  plot(s2)
-  ylabel("s\"")
-  xlabel("t(sec)")
+  figure(4);
+  subplot(1,2,1);
+  plot(s2);
+  ylabel("s\"");
+  xlabel("t(sec)");
   
-  subplot(1,2,2)
-  plot(s1)
-  title("Euler method s' ")
-  ylabel("s'")
-  xlabel("t(sec)")
-  plot(s1)
+  subplot(1,2,2);
+  title("Euler method s' ");
+  plot(t,s1);
+  ylabel("s'");
+  xlabel("t(sec)");
+  
   
 
-  for k = 1 : length(t)*(t)
-    s(k+1) = s(k)+h*s1(k)
+  
+  for k = 1 :30000
+    s(k+1) = s(k)+h*s1(k);
   endfor
-  figure(5)
-  plot(s)
-  title("Euler Method s")
-  ylabel("s")
-  xlabel("t(sec)")
+  figure(5);
+  plot(t,s);
+  title("Euler Method s");
+  ylabel("s");
+  xlabel("t(sec)");
+  
   
   # ---------------- Rational Move ---------------------- #
   
-  w(1) = 0
-  for k = 1: length(t)*(t)
-    w1(k)=((d/2*(f2-f1) - bu*abs(w(k))*w(k)))/Iz
-    w(k+1) = w(k)+h*w1(k)
+  w(1) = 0;
+  for k = 1: 30000
+    w1(k)=((d/2*(f2-f1) - bu*abs(w(k))*w(k)))/Iz;
+    w(k+1) = w(k)+h*w1(k);
   endfor
   # plot w1 and w #
 
-  figure(6)
+  figure(6);
 
-  subplot(1,2,1)
+  subplot(1,2,1);
   
-  plot(w1)
-  ylabel("w'")
-  xlabel("t(sec)")
+  plot(w1);
+  ylabel("w'");
+  xlabel("t(sec)");
 
-  plot(w)
-  title("Euler Method w")  
-  ylabel("w")
-  xlabel("t(sec)")
-
+  plot(t,w);
+  title("Euler Method w");  
+  ylabel("w");
+  xlabel("t(sec)");
+  
+  
   endfunction
--- ImprovedEuler.m
+++ ImprovedEuler.m
@@ -3,85 +3,88 @@ function ImprovedEuler
   #----------- First -----------#
 
   #----------- Data 1----------#
-  f1 = 4835/7000
-  f2 = 4835/7000
-  m = 9
-  d=1
-  h=0.0001
-  Iz= 0.038
-  s0 = 4835/1000
-  AM = 4835
-  bs = 3 - (4835/5000)
-  bu = 5 - (4835/5000)
+  f1 = 4835/7000;
+  f2 = 4835/7000;
+  m = 9;
+  d=1;
+  h=0.001;
+  Iz= 0.038;
+  s0 = 4835/1000;
+  AM = 4835;
+  bs = 3 - (4835/5000);
+  bu = 5 - (4835/5000);
 
   
-  t = 0:0.1:30
-  display(length(t))
-  s1=0
+  t = 0:0.001:30;
+  display(length(t));
+  s1=0;
   #s2= [(f1+f2) - bs*abs(s1)*s1]/9
-  s = zeros(1,length(t))
-  display(length(s))
-  s1 = zeros(1,length(t))
-  s2 = zeros(1,length(t))
-  s1(1) = 0 
-  s(1)=AM/4835
+  s = zeros(1,30000);
+  s1 = zeros(1,30000);
+  s2 = zeros(1,30000);
+  s1(1) = 0 ;
+  s(1)=AM/4835;
   
   # -------- ImprovedEuler 1 -------- #
   
   # ---------- Trasportation Move ---------- #
   # s' #
-  for k = length(t)*(t+1)
-    s2(k) = (f1+f2 -bs*(abs(s1(k)*s1(k))))/m
-    s1(k+1)= s1(k)+h/2*(s2(k)+(f1+f2/m - (abs((s1(k)+h*(s2(k))))*(s1(k)+h*(s2(k))))/m)
+  for k = 1:30000
+    s2(k) = ((f1+f2) -bs*((abs(s1(k))*s1(k))))/m;
+    #s1(k+1)= s1(k)+h/2*(s2(k)+(f1+f2/m - (abs((s1(k)+h*(s2(k))))*(s1(k)+h*(s2(k))))/m)
+    s1(k+1) = s1(k)+h/2*(s2(k)+(f1+f2)/m-(bs*abs(s1(k)+h*s2(k))*(s1(k)+h*s2(k)))/m);
   endfor
   
   # s #
   
-  for k = length(t)*(t)
-    s(k+1) = s(k)+h/2(s1(k)+s1(k+1))
+  for k = 1:30000
+    s(k+1) = s(k)+h/2*(s1(k)+s1(k+1));
   endfor
   
   # --------- Rational Move 1 --------- #
-  w = zeros(1,length(t))
-  w1 = zeros(1,length(t))
+  w = zeros(1,30000);
+  w1 = zeros(1,30000);
+  w1(1)=0;
   # w' and w #
   
-  for k = length(t)*(t)
-    w1(k) = (d/2*(f2-f1)-bu*abs(w(k))*w(k))/Iz
-    w(k) = w(k)+h/2*(w1(k)+(d/2*(f2-f1)-bu*abs(w(k)+w1(k))*(w(k)+w1(k)))/Iz)
+  for k =1:30000
+    w1(k) = ((d/2*(f2-f1)-bu*abs(w(k))*w(k))/Iz);
+    #w(k) = w(k)+h/2*(w1(k)+(d/2*(f2-f1)-bu*abs(w(k)+w1(k))*(w(k)+w1(k)))/Iz)
+    w(k+1)= w(k)+h/2*(w1(k)+h*(w1(k)+((d/2*(f2-f1)-bu*abs(w(k)+w1(k))*(w(k)+w1(k)))/Iz)));
   endfor
+  #}
   
   #---------- SUBPLOT 1----------#
-
+  
   figure(1)
   subplot(1,2,1)
-  plot(s1)
+  plot(t,s1)
   title("Improved method s'")
   ylabel("s'")
   xlabel("t(sec)")
-
+  
   subplot(1,2,2)
-  plot(s)
+  plot(t,s)
   title("Improved Method Euler s")
   ylabel("s'")
   xlabel("t(sec)")
   
-  
+ 
   figure(2)
   subplot(1,2,1)
   plot(w1)
   ylabel("w'")
   xlabel("t(sec)")
-  
+
   subplot(1,2,2)
-  plot(w)
+  
+  plot(t,w)
+  title("Improved Method Euler w")
   ylabel("w")
   xlabel("t(sec)")
     
 
-
-
-
+  #{
   # ----------- Second Input Values (f1+f2) -------------#
 
 
@@ -162,7 +165,7 @@ function ImprovedEuler
   plot(w)
   ylabel("w")
   xlabel("t(sec)")
-  
+  #}
 endfunction
 
   
